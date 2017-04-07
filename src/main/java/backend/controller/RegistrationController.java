6f1a09245562040dd94276626188f33444a9cb5e
pekinek <pekinhere@gmail.com>
2017-04-07 22:22:21 +0200

    Initial commit

---
+++ src/main/java/backend/controller/RegistrationController.java
@@ -0,0 +1,52 @@
+package backend.controller;
+
+import backend.exceptions.InvalidPhoneNumberException;
+import backend.exceptions.UserExistsException;
+import backend.model.User;
+import backend.repository.UserRepository;
+import backend.service.EmailService;
+
+import org.apache.commons.codec.digest.DigestUtils;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.MessageDigest;
+import java.util.UUID;
+
+@RestController
+public class RegistrationController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    EmailService emailService;
+
+    @CrossOrigin
+    @RequestMapping("/register")
+    public ResponseEntity<String> register(@RequestBody User user) throws UserExistsException, InvalidPhoneNumberException {
+        if(!userRepository.findByLogin(user.getLogin()).isEmpty()){
+            throw new UserExistsException();
+        }
+        if(!StringUtils.isNumeric(user.getTelephone())){
+            throw new InvalidPhoneNumberException();
+        }
+        user.setType("normal");
+        user.setToken(UUID.randomUUID().toString());
+        user.setEnabled(true);
+        String password = RandomStringUtils.randomAlphanumeric(10);
+        user.setPassword(DigestUtils.sha512Hex(password));
+        userRepository.save(user);
+        emailService.sendEmail(user, password);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
---
+++ src/main/java/backend/model/User.java
@@ -0,0 +1,47 @@
+package backend.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+@Entity
+@Setter
+@Getter
+public class User {
+
+    @Id
+    @NotNull
+    @Size(min=1, max=30)
+    private String login;
+    @NotNull
+    @JsonProperty(access = Access.WRITE_ONLY)
+    private String token;
+    @NotNull
+    @JsonProperty(access = Access.WRITE_ONLY)
+    @Column(length = 1000)
+    private String password;
+    @NotNull
+    @Size(min=1, max=50)
+    private String email;
+    @NotNull
+    @Size(min=6, max=15)
+    private String telephone;
+    @NotNull
+    @Size(min=1, max=50)
+    private String firstName;
+    @NotNull
+    @Size(min=1, max=50)
+    private String lastName;
+    @NotNull
+    private String type;
+    @NotNull
+    private Boolean enabled;
+}
